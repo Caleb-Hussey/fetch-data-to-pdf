fe5c993285385148bf709cc9ed3807570458c919
DESKTOP-2B8I1JF\sempe <caleb.hussey@gmail.com>
2025-09-16 07:32:12 +0200

    Update document details

--- src/main/java/nfl/App.java
+++ src/main/java/nfl/App.java
@@ -16,9 +16,9 @@ public class App
         PdfMaker pdfMaker = new PdfMaker();
         SpreadFetcher spreadFetcher = new SpreadFetcher();
         CsvConverter converter = new CsvConverter();
-        String fetchHeader = "Week 2";
-        String filename = "NFL Week 2 Spreads.pdf";
-        String title = "NFL Week 2 Spreads";
+        String fetchHeader = "Week 3";
+        String filename = "NFL Week 3 Spreads.pdf";
+        String title = "NFL Week 3 Spreads";
         boolean shouldRefetchSpreads = true;
         int horizontal_line_position = 1;
         WeeklyData data = null;
